17ee365c2548b9e6302a69acae809462be8338af
Alfie Champion <alfie.champion@f-secure.com>
2020-05-10 13:22:46 +0100

    initial commit for dropbox channel

---
+++ Src/Common/FSecure/C3/Interfaces/Channels/Dropbox.cpp
@@ -0,0 +1,97 @@
+#include "Stdafx.h"
+#include "Dropbox.h"
+#include "Common/FSecure/Crypto/Base64.h"
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+FSecure::C3::Interfaces::Channels::Dropbox::Dropbox(ByteView arguments)
+	: m_inboundDirectionName{ arguments.Read<std::string>() }
+	, m_outboundDirectionName{ arguments.Read<std::string>() }
+{
+	auto [DropboxToken, channelName] = arguments.Read<std::string, std::string>();
+	m_dropboxObj = FSecure::Dropbox{ DropboxToken, channelName };
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+size_t FSecure::C3::Interfaces::Channels::Dropbox::OnSendToChannel(ByteView data)
+{
+	//Create a filename thats prefixed with message direction and more granular timestamp for querying later
+	std::string ts = std::to_string(FSecure::Utils::TimeSinceEpoch());
+	std::string filename = m_outboundDirectionName + OBF("-") + FSecure::Utils::GenerateRandomString(10) + OBF("-") + ts; // +OBF(".txt");
+
+	// There is a cap on uploads of files >150mb at which point different APIs are required, left unimplemented for now.
+	m_dropboxObj.WriteMessageToFile(filename, data);
+	return data.size();
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+std::vector<FSecure::ByteVector> FSecure::C3::Interfaces::Channels::Dropbox::OnReceiveFromChannel()
+{
+	// Fetch inbound messages as a map of ids and ISO timestamps
+	std::map<std::string, std::string> messages = m_dropboxObj.GetMessagesByDirection(m_inboundDirectionName);
+	std::vector<std::string> repliesTs;
+
+	// We can't fetch directly with an ISO client_modified value, so we're pulling out the date 
+	// strings to iterate over and using that as a key to fetch the file by id.
+	for (std::map<std::string, std::string>::iterator it = messages.begin(); it != messages.end(); ++it) {
+		repliesTs.push_back(it->first);
+	}
+
+	std::vector<ByteVector> ret;
+
+	//Read the files in order and delete when data is retrieved.
+	for (std::vector<std::string>::iterator ts = repliesTs.begin(); ts != repliesTs.end(); ++ts)
+	{
+		std::string messagePath = messages[*ts];
+		auto fileContent = m_dropboxObj.ReadFile(messagePath);
+
+		m_dropboxObj.DeleteFile(messagePath);
+		ret.emplace_back(std::move(fileContent));
+	}
+	return ret;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const char* FSecure::C3::Interfaces::Channels::Dropbox::GetCapability()
+{
+	return R"_(
+{
+	"create":
+	{
+		"arguments":
+		[
+			[
+				{
+					"type": "string",
+					"name": "Input ID",
+					"min": 4,
+					"randomize": true,
+					"description": "Used to distinguish packets for the channel"
+				},
+				{
+					"type": "string",
+					"name": "Output ID",
+					"min": 4,
+					"randomize": true,
+					"description": "Used to distinguish packets from the channel"
+				}
+			],
+			{
+				"type": "string",
+				"name": "Dropbox token",
+				"min": 1,
+				"description": "This token is what channel needs to interact with Dropbox's API"
+			},
+			{
+				"type": "string",
+				"name": "Folder name",
+				"min": 4,
+				"randomize": true,
+				"description": "Folder to create for channel"
+			}
+		]
+	},
+	"commands": []
+}
+)_";
+}
